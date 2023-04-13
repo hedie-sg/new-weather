0f6b1264357ef6253a17709253110ee93ae11647
hedie-sg <hedi84.ghasemi@gmail.com>
2023-04-13 17:46:01 +0430

    added the current date

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+function newDate(timetemp) {
+    let date = new Date(timetemp);
+
+    let days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+    ];
+    let day = days[date.getDay()];
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
     console.log(response.data);
     let temptElement = document.querySelector('#tempt');
@@ -10,6 +35,8 @@ function displayTemperature(response) {
     humidityElement.innerHTML = `Humidity: ${response.data.temperature.humidity}%`;
     let windElement = document.querySelector('#wind');
     windElement.innerHTML = `wind: ${Math.round(response.data.wind.speed)}Km/h`;
+    let dateElement = document.querySelector('#date');
+    dateElement.innerHTML = newDate(response.data.time * 1000);
 }
 
 let apiKey = '2o0f65b4a41b4138505bc7t3f2fa0460';
