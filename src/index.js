7731683e644b6de7d7f11f59bc6cb27a0ed0f4a1
hedie-sg <hedi84.ghasemi@gmail.com>
2023-04-13 17:16:03 +0430

    added current tempt

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+    console.log(response.data);
+    let temptElement = document.querySelector('#tempt');
+    temptElement.innerHTML = Math.round(response.data.temperature.current);
+    let cityElement = document.querySelector('#city');
+    cityElement.innerHTML = response.data.city;
+    let descriptElement = document.querySelector('#description');
+    descriptElement.innerHTML = response.data.condition.description;
+    let humidityElement = document.querySelector('#humidity');
+    humidityElement.innerHTML = `Humidity: ${response.data.temperature.humidity}%`;
+    let windElement = document.querySelector('#wind');
+    windElement.innerHTML = `wind: ${Math.round(response.data.wind.speed)}Km/h`;
+}
 
+let apiKey = '2o0f65b4a41b4138505bc7t3f2fa0460';
+
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=paris&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
